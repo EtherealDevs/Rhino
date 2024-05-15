fa4748d55cdc03af38cd9f0aa238a234c5cc5bc8
joaccco <joaccofranc02@icloud.com>
2024-05-15 17:16:37 -0300

    Vista Contact

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,55 +1,61 @@
 @extends('layouts.app')
 @section('content')
- 
-<div class="relative flex min-h-screen flex-col justify-center overflow-hidden sm:py-12">
-    <div class="bg-white max-w-4xl mx-auto w-full">
-      <div class="grid grid-cols-6 h-full">
-        <div class="bg-blue-900 p-10 col-span-2">
-          <h2 class="mb-10 font-bold text-2xl text-blue-100 before:block before:absolute before:bg-sky-300 before:content[''] relative before:w-20 before:h-1 before:-skew-y-3 before:-bottom-4">Infos Contact</h2>
-          <p class="font-bold text-blue-100 py-8 border-b border-blue-700">
-            Location Address
-            <span class="font-normal text-xs text-blue-300 block">Romada, 16/A, YoYo City, USA</span>
-          </p>
-          <p class="font-bold text-blue-100 py-8 border-b border-blue-700">
-            Phone Number
-            <span class="font-normal text-xs text-blue-300 block">+889 (909) 567 87 9</span>
-          </p>
-          <p class="font-bold text-blue-100 py-8 border-b border-blue-700">
-            Email Address
-            <span class="font-normal text-xs text-blue-300 block">example@example.com</span>
-          </p>
-          <p class="font-bold text-blue-100 py-8 border-b border-blue-700">
-            Web Address
-            <span class="font-normal text-xs text-blue-300 block">zigzagexampl.com</span>
-          </p>
-  
+    <div class="h-screen flex">
+        <div class="w-screen mx-auto">
+            <div>
+              <div>
+                <h2 class="absolute flex mx-auto w-full text-3xl font-extrabold uppercase mt-12 justify-center">
+                    Pagina de  <span class="text-white ml-3">Contacto</span> 
+                </h2>
+              </div>
+                
+                <div class="grid grid-cols-2 h-screen">
+
+                    <div class="bg-white">
+                        h
+                    </div>
+
+                    <div class="bg-blue-600">
+                        <div class=" items-center justify-end">
+                            <div class="flex mx-auto my-auto h-full w-full max-w-lg">
+                                <p class="text-white mt-3">Email us at help@domain.com or message us here:</p>
+
+                                <form action="https://api.web3forms.com/submit" class="mt-10">
+
+                                    <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+                                    <div class="grid gap-6 sm:grid-cols-1">
+                                        <div class="relative z-0">
+                                            <input type="text" name="name"
+                                                class="peer block w-full appearance-none border-0 border-b border-white bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-200 focus:outline-none focus:ring-0"
+                                                placeholder=" " />
+                                            <label
+                                                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-white duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-200">Nombre y Apellido
+                                            </label>
+                                        </div>
+                                        <div class="relative z-0">
+                                            <input type="text" name="email"
+                                                class="peer block w-full appearance-none border-0 border-b border-white bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-200 focus:outline-none focus:ring-0"
+                                                placeholder=" " />
+                                            <label
+                                                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-white duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-200 peer-focus:dark:text-blue-500">Tu
+                                                email</label>
+                                        </div>
+                                        <div class="relative z-0 col-span-1">
+                                            <textarea name="message" rows="5"
+                                                class="peer block w-full appearance-none border-0 border-b border-white bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-200 focus:outline-none focus:ring-0"
+                                                placeholder=" "></textarea>
+                                            <label
+                                                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-white duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-200 peer-focus:dark:text-blue-500">Tu
+                                                mensaje</label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="mt-5 rounded-md bg-white px-10 py-2 text-black">Enviar Mensaje</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="bg-blue-50 p-14 col-span-4">
-          <h2 class="mb-14 font-bold text-4xl text-blue-900 before:block before:absolute before:bg-sky-300 before:content[''] relative before:w-20 before:h-1 before:-skew-y-3 before:-bottom-4">Entrer en contact</h2>
-          <div class="grid gap-6 mb-6 grid-cols-2">
-            <div class="flex flex-col">
-              <input class="py-4 bg-white rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Votre nom" ></inpu>
-            </div>
-            <div class="flex flex-col">
-              <input class="py-4 bg-white rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Votre prénom" ></inpu>
-            </div>
-          </div>
-          <div class="grid gap-6 mb-6 grid-cols-2">
-            <div class="flex flex-col">
-              <input class="py-4 bg-white rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Email Adresse" ></inpu>
-            </div>
-            <div class="flex flex-col">
-              <input class="py-4 bg-white rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Sujet" ></inpu>
-            </div>
-          </div>
-          <div class="mb-6">
-            <textarea class="w-full rounded-2xl placeholder:text-xs px-6 py-4" placeholder="Votre message ici" name="" id="" rows="8"></textarea>
-          </div>
-          <div class="flex justify-center">
-            <button class="rounded-full bg-blue-900 text-white font-bold py-4 px-6 min-w-40 hover:bg-blue-800 transition-all">Valider</button>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 @endsection
