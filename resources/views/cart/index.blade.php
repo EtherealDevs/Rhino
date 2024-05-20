c6ac639243bf2de52f5a64c70c74f6ff26859ba0
Mateodans <mateodand@gmail.com>
2024-05-20 11:58:45 -0300

    cart incompleto

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -3,38 +3,41 @@
     <!-- component -->
     <div class="w-full h-full bg-gradient-to-r from-gray-50 to-blue-100 via-gray-100 top-0 overflow-y-auto overflow-x-hidden fixed sticky-0"
         id="chec-div">
-        <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
-        <div class="w-full absolute z-10 right-0 h-full overflow-x-hidden transform translate-x-0 transition ease-in-out duration-700"
+        <div class="w-full absolute z-10 right-0 overflow-x-hidden transform translate-x-0 transition ease-in-out duration-700"
             id="checkout">
             <div class="flex items-end lg:flex-row flex-col justify-end mr-10 mt-10" id="cart">
                 <div
-                    class="lg:w-4/12 md:w-8/12 w-full rounded-lg shadow-2xl  bg-gradient-to-b via-[#2E3366] from-[#343678] to-[#273053] h-full">
+                    class="lg:w-4/12 md:w-8/12 w-full rounded-lg shadow-2xl bg-gradient-to-b via-[#2E3366] from-[#343678] to-[#273053] ">
                     <div
                         class="flex flex-col h-3/4 rounded-lg lg:px-8 md:px-7 px-4 lg:py-20 md:py-10 py-6 justify-between overflow-y-auto">
                         <form class="flex flex-wrap gap-3 w-full p-5">
                             <p class="text-center font-bold font-josefin text-white text-3xl">Informacion de
                                 pago
                             </p>
-                            <div class="mb-5">
-                                <label for="name"
-                                    class="block mb-2 text-sm font-josefin text-gray-900 dark:text-white">Nombre
-                                    y apellido</label>
-                                <input type="name" id="name"
-                                    class="rounded-md peer px-2 py-2 border-2 border-gray-200 placeholder-gray-300"
-                                    placeholder="Zanabria Reyes Joaquin" required />
+                            <div class="grid col-span-2">
+                                <div class="mb-5 col">
+                                    <label for="name"
+                                        class="block mb-2 text-sm font-josefin text-gray-900 dark:text-white">Nombre
+                                        y apellido</label>
+                                    <input type="name" id="name"
+                                        class="rounded-md peer px-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                                        placeholder="Zanabria Reyes Joaquin" required />
+                                </div>
+                                <div class="col">
+                                    <label class="relative w-full flex flex-col">
+                                        <span class="font-josefin text-white mb-3">Card number</span>
+                                        <input
+                                            class="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
+                                            type="text" name="card_number" placeholder="0000 0000 0000" />
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6"
+                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                                        </svg>
+                                    </label>
+                                </div>
                             </div>
-                            <label class="relative w-full flex flex-col">
-                                <span class="font-josefin text-white mb-3">Card number</span>
-                                <input class="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
-                                    type="text" name="card_number" placeholder="0000 0000 0000" />
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                    class="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                                </svg>
-                            </label>
-
                             <label class="relative flex-1 flex flex-col">
                                 <span class="font-josefin text-white mb-3">Expire date</span>
                                 <input class="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
@@ -46,7 +49,6 @@
                                         d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                 </svg>
                             </label>
-
                             <label class="relative flex-1 flex flex-col">
                                 <span class="font-josefin text-white flex items-center gap-3 mb-3">
                                     CVC/CVV
