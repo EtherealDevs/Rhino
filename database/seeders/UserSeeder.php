9ce83b8c7ea3ddd697f498ad7fff1e1c6093c9ad
joaccco <joaccofranc02@icloud.com>
2024-05-21 21:33:07 -0300

    Cambios esteticos

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin1',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('1234qwer')
+        ]);
+    }
+}
