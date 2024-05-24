6eec891273ff398d49665ff52cc6dda95324774e
joaccco <joaccofranc02@icloud.com>
2024-05-24 12:13:44 -0300

    Vistas Admin/Combo

--- resources/views/admin/combos/create.blade.php
+++ resources/views/admin/combos/create.blade.php
@@ -50,15 +50,14 @@
                                 <div class="relative z-0 w-full mb-5">
                                     <input type="text" name="name" placeholder=" " required
                                         class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" />
-                                    <label for="name" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Productos</label>
+                                    <label for="name" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Nombre</label>
                                     <span class="text-sm text-red-600 hidden" id="error">Este Campo es requerido</span>
                                 </div>
     
                                 <div class="relative z-0 w-full mb-5">
                                     <input type="text" name="name" placeholder=" " required
                                         class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" />
-                                    <label for="slug" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Slug
-                                        Amigable</label>
+                                    <label for="slug" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Productos</label>
                                     <span class="text-sm text-red-600 hidden" id="error">Este Campo es requerido</span>
                                 </div>
     
@@ -66,7 +65,7 @@
                                     <input type="text" name="name" placeholder=" " required
                                         class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" />
                                     <label for="description"
-                                        class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Descripcion</label>
+                                        class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Descuento %</label>
                                     <span class="text-sm text-red-600 hidden" id="error">Este Campo es requerido</span>
                                 </div>
     
@@ -85,25 +84,6 @@
                                         class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Seleccionar el
                                         padre</label>
                                     <span class="text-sm text-red-600 hidden" id="error">Option has to be selected</span>
-                                </div>
-    
-                                <div class="mb-8">
-                                    
-                                    <label for="file"
-                                        class="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center">
-                                        <div>
-                                            <span class="mb-2 block text-xl font-semibold text-[#07074D]">
-                                                Puedes Arrastrar archivos aqui
-                                            </span>
-                                            <span class="mb-2 block text-base font-medium text-[#6B7280]">
-                                                o
-                                            </span>
-                                            <span
-                                                class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
-                                                <input type="file" name="image" accept="image/*" id="image" />
-                                            </span>
-                                        </div>
-                                    </label>
                                 </div>
     
                                 <button id="button" type="submit"
