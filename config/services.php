72310c38fadaee66b6857238653835e346ec127a
Mateodans <mateodand@gmail.com>
2024-06-12 18:08:42 -0300

    Home Page Responsive (Joacco)

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         ],
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
 ];
