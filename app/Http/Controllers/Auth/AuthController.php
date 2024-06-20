79de4564e928991d475e0c28425bca5dd5431938
Mateodans <90775644+Mateodans@users.noreply.github.com>
2024-06-20 16:57:28 -0300

    cambios varios

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
 
     public function callbackFacebook(){
         if(!isset($_GET['error'])){
-        $user = Socialite::driver('facebook')->user();
+        $user = Socialite::driver('facebook')->stateless()->user();
 
         $user = User::firstOrCreate([
             'email' => $user->getEmail(),
@@ -34,7 +34,7 @@ class AuthController extends Controller
 
     public function callbackGoogle(){
         if(!isset($_GET['error'])){
-            $user = Socialite::driver('google')->user();
+            $user = Socialite::driver('google')->stateless()->user();
             $user = User::firstOrCreate([
                 'email' => $user->getEmail(),
             ], [
