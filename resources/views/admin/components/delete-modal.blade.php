ed85459f43d89be0ab582c3c37770bf11df9f92f
kevin <kevin.galaxy04@gmail.com>
2024-08-06 12:06:58 -0300

    "Removed delete-modal.blade.php and updated index.blade.php to include a new delete-modal component"

--- resources/views/admin/components/delete-modal.blade.php
+++ resources/views/admin/components/delete-modal.blade.php
@@ -1,5 +1,5 @@
-<form action={{ route('admin.combos.destroy', $combo) }} method="POST">
-<div id="default-modal-{{$combo->id}}" tabindex="-1" aria-hidden="true" class="hidden min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
+<form action={{ route($route, $item) }} method="POST">
+<div id="default-modal-{{$item->id}}" tabindex="-1" aria-hidden="true" class="hidden min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
     <div class="absolute bg-black opacity-80 inset-0 z-0"></div>
  <div class="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
    <!--content-->
@@ -18,13 +18,13 @@
      </div>
      <!--footer-->
      <div class="p-3  mt-2 text-center space-x-4 md:block">
-         <button data-modal-hide="default-modal-{{$combo->id}}" type="button" class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+         <button data-modal-hide="default-modal-{{$item->id}}" type="button" class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
              Cancelar
          </button>
 
             @csrf
             @method('DELETE')
-            <button data-modal-hide="default-modal-{{$combo->id}}" type="submit" class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Eliminar</button>
+            <button data-modal-hide="default-modal-{{$item->id}}" type="submit" class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Eliminar</button>
 
      </div>
    </div>
