33d8b1d905e1101ea103c33ff0dc8679dbc04fac
joaccco <joaccofranc02@icloud.com>
2024-08-18 19:57:35 -0300

    Admin Index (Orders LIst)

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -296,43 +296,48 @@
 
                             </div>
  --}}
- @if ($pendingOrders->isEmpty())
- <p>No hay pedidos pendientes.</p>
-@else
- <ul>
-     @foreach ($pendingOrders as $order)
-         <!-- Item #1 -->
-         <div class="relative pl-8 sm:pl-32 py-6 group">
-             <!-- Purple label -->
-             <div class="font-medium text-indigo-500 mb-1 sm:mb-0">Pedido #{{ $order->id }} - Corrientes, Capital</div>
-             <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
-             <div
-                 class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                 <div>
-                     <time
-                     class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">{{ $order->created_at->format('d-m-Y') }}</time>
-                     <br>
-                     <time
-                     class="mt-2 sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">{{ $order->created_at->format('h:m') }}</time>
-                 </div>
-                 
-                 <div class="text-xl font-bold text-slate-900">Acme was founded in Milan, Italy - <strong class=""><a href="{{ route('admin.orders.show', $order->id) }}"
-                     class="text-blue-600 hover:underline">Ver detalles →</a></strong>
-                 </div>
-             </div>
-             <!-- Content -->
-             <div class="text-slate-500">Pretium lectus quam id leo. Urna et pharetra pharetra
-                 massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus
-                 risus.</div>
-         </div>
-     @endforeach
- </ul>
-@endif
+                            @if ($pendingOrders->isEmpty())
+                                <p>No hay pedidos pendientes.</p>
+                            @else
+                                <ul>
+                                    @foreach ($pendingOrders as $order)
+                                        <!-- Item #1 -->
+                                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                                            <!-- Purple label -->
+                                            <div class="font-medium text-indigo-500 mb-1 sm:mb-0">Pedido
+                                                #{{ $order->id }} - Corrientes, Capital</div>
+                                            <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
+                                            <div
+                                                class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                                <div>
+                                                    <time
+                                                        class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">{{ $order->created_at->format('d-m-Y') }}</time>
+                                                    <br>
+                                                    <time
+                                                        class="mt-2 sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-gray-600 bg-gray-100 rounded-full">{{ $order->created_at->format('h:m') }}</time>
+                                                </div>
+
+                                                <div class="text-xl font-bold text-slate-900">${{ $order->total }} -
+                                                    <strong class=""><a
+                                                            href="{{ route('admin.orders.show', $order->id) }}"
+                                                            class="text-blue-600 hover:underline">Ver detalles
+                                                            →</a></strong>
+                                                </div>
+                                            </div>
+                                            <!-- Content -->
+                                            <div class="text-slate-500">
+                                                <p>Nombre: {{ $order->user->name }}</p>
+                                                <p>Email: {{ $order->user->email }}</p>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </ul>
+                            @endif
 
                         </div>
                     </div>
 
-                   
+
 
                 </div>
             </div>
