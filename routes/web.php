6b0ef6851837d5892db5f30a507bb093a59b772d
joaccco <joaccofranc02@icloud.com>
2024-09-06 18:00:04 -0300

    Filter sizes

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,60 @@ use App\Http\Controllers\OrderController;
 
 use App\Livewire\ShippingCost;
 
+/* Rutas Normales */
 Route::get('/', [HomeController::class, 'index']);
 
-// web.php o api.php
-Route::post('/calcular-envio', [ShippingCost::class, 'calcularEnvio']);
+Route::get('/contact', function () {
+    return view('contact.index');
+});
+
+/* Formulario de Contacto */
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
+
+Route::get('/about', function () {
+    return view('about.index');
+});
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}/{productItem}', [ProductController::class, 'show'])->name('products.show');
+Route::post('products/{product}/{productItem}', [ProductController::class, 'addToCart'])->name('products.addToCart');
+
+Route::get('promos', [PromoController::class, 'index'])->name('promos.index');
+
+// Esta ruta se usa para filtrar productos por categoría
+Route::get('/filter', [ProductController::class, 'filter'])->name('products.filter');
+
+Route::resource('/combos', ComboController::class)->names('combos');
+// Route::get('/combos/show', [ComboController::class, 'show'])->name('combos.show');
+
+// Ruta para mostrar todos los pedidos
+Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
+
+
+
+// Ruta para mostrar un pedido específico
+Route::get('orders/{id}', [OrderController::class, 'show'])->name('orders.show');
+
+
+Route::get('/test', [TestController::class, 'index'])->name('test');
+
+
+Route::get('/products/show', function () {
+    return view('products.show');
+});
+
+
+/* Autenticacion */
+Route::get('/auth/redirect/facebook', [AuthController::class, 'redirectFacebook'])->name('auth.redirect.facebook');
+Route::get('/auth/callback/facebook', [AuthController::class, 'callbackFacebook'])->name('auth.callback.facebook');
+
+Route::get('/auth/redirect/google', [AuthController::class, 'redirectGoogle'])->name('auth.redirect.google');
+Route::get('/auth/callback/google', [AuthController::class, 'callbackGoogle'])->name('auth.callback.google');
+
+
+Route::get('/eliminacion-datos', function () {
+    return view('eliminacion-datos.index');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::post('/favorites/add/{product}', [Navigation::class, 'add'])->name('favorites.add');
@@ -39,20 +89,17 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/firebase', [FirebaseController::class, 'index']);
 
 
-Route::get('/contact', function () {
-    return view('contact.index');
-});
 
-Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
+/* Carrito Envios y Pago */
+Route::get('/enviowa', [WhapController::class, 'envio']);
 
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::get('/cart/envio', [CartController::class, 'envio'])->name('cart.envio');
+Route::post('/cart', [CartController::class, 'addToCart'])->name('cart.addItem');
+Route::post('/cartCombo', [CartController::class, 'addComboToCart'])->name('cart.addCombo');
+Route::delete('/cart/{item}', [CartController::class, 'removeFromCart'])->name('cart.removeItem');
+Route::delete('/cart', [CartController::class, 'dropCart'])->name('cart.dropCart');
 
-Route::get('/eliminacion-datos', function () {
-    return view('eliminacion-datos.index');
-});
-
-Route::get('/about', function () {
-    return view('about.index');
-});
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/checkout/delivery', [CheckoutController::class, 'showCheckoutDeliveryPage'])->name('checkout.delivery');
@@ -65,46 +112,8 @@ Route::get('/successfullyPaid', function () {
     return view('payments.successfullyPaid');
 });
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{product}/{productItem}', [ProductController::class, 'show'])->name('products.show');
-Route::post('products/{product}/{productItem}', [ProductController::class, 'addToCart'])->name('products.addToCart');
+Route::post('/calcular-envio', [ShippingCost::class, 'calcularEnvio']);
 
-// Esta ruta se usa para filtrar productos por categoría
-Route::get('/category', [ProductController::class, 'category'])->name('products.category');
-
-Route::resource('/combos', ComboController::class)->names('combos');
-// Route::get('/combos/show', [ComboController::class, 'show'])->name('combos.show');
-
-
-// Ruta para mostrar todos los pedidos
-Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
-
-Route::get('promos', [PromoController::class, 'index'])->name('promos.index');
-
-// Ruta para mostrar un pedido específico
-Route::get('orders/{id}', [OrderController::class, 'show'])->name('orders.show');
-
-
-Route::get('/test', [TestController::class, 'index'])->name('test');
-
-Route::get('/cart', [CartController::class, 'index'])->name('cart');
-Route::get('/cart/envio', [CartController::class, 'envio'])->name('cart.envio');
-Route::post('/cart', [CartController::class, 'addToCart'])->name('cart.addItem');
-Route::post('/cartCombo', [CartController::class, 'addComboToCart'])->name('cart.addCombo');
-Route::delete('/cart/{item}', [CartController::class, 'removeFromCart'])->name('cart.removeItem');
-Route::delete('/cart', [CartController::class, 'dropCart'])->name('cart.dropCart');
-
-Route::get('/products/show', function () {
-    return view('products.show');
-});
-
-Route::get('/auth/redirect/facebook', [AuthController::class, 'redirectFacebook'])->name('auth.redirect.facebook');
-Route::get('/auth/callback/facebook', [AuthController::class, 'callbackFacebook'])->name('auth.callback.facebook');
-
-Route::get('/auth/redirect/google', [AuthController::class, 'redirectGoogle'])->name('auth.redirect.google');
-Route::get('/auth/callback/google', [AuthController::class, 'callbackGoogle'])->name('auth.callback.google');
-
-Route::get('/enviowa', [WhapController::class, 'envio']);
 
 /* Rutas de ADMIN */
 // Route::get('/admin', function () {
