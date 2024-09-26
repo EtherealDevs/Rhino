95d1d3dd9b566ebd573b8142ae772ab1fda1ee05
Mateodans <mateodand@gmail.com>
2024-09-25 22:18:11 -0300

    admin/orders

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -10,7 +10,8 @@
                 <h2 class="text-2xl font-semibold text-gray-800 mb-4">Información del Usuario</h2>
                 <p class="text-lg text-gray-600"><strong class="font-medium">Nombre:</strong> {{ $order->user->name }}</p>
                 <p class="text-lg text-gray-600"><strong class="font-medium">Email:</strong> {{ $order->user->email }}</p>
-                <p class="text-lg text-gray-600"><strong class="font-medium">Teléfono:</strong> {{ $order->user->phone_number }}
+                <p class="text-lg text-gray-600"><strong class="font-medium">Teléfono:</strong>
+                    {{ $order->user->phone_number }}
                 </p>
             </div>
 
@@ -118,8 +119,9 @@
                         </a>
 
                         <!-- Botón de contacto por WhatsApp -->
-                        <a href="https://wa.me/{{ $order->user->phone_number }}?text=Hola%20{{ $order->user->name }},%20te%20contacto%20en%20relación%20al%20pedido%20#{{ $order->id }}."
-                            class="flex items-center justify-center gap-3 mb-4 text-white bg-green-500 hover:bg-green-600 rounded-full px-4 py-3 transition-all">
+                        <a href="https://wa.me/{{ $order->user->phone_number }}?text=Hola%20{{ $order->user->name }},%20te%20contacto%20en%20relación%20al%20pedido%20numero%20{{ $order->id }}."
+                            class="flex items-center justify-center gap-3 mb-4 text-white bg-green-500 hover:bg-green-600 rounded-full px-4 py-3 transition-all"
+                            target="blank_">
                             <div class="bg-white p-2 rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500" width="24" height="24"
                                     viewBox="0 0 24 24">
