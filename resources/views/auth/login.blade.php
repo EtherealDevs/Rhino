eef1038a775bd3641361f65e18e5b8f01e041981
joaccco <joaccofranc02@icloud.com>
2024-11-12 09:32:13 -0300

    google changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -92,23 +92,6 @@
                                 </svg>
                                 <span>Google</span>
                             </a>
-
-                            <a href="{{ route('auth.redirect.facebook') }}"
-                                class="w-full flex items-center justify-center mb-6 md:mb-0 border border-gray-300 hover:border-gray-900 hover:bg-gray-900 text-sm text-gray-500 p-3  rounded-lg tracking-wide font-medium  cursor-pointer transition ease-in duration-500 px-">
-                                <svg class="w-4 mr-2" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100"
-                                    xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
-                                    <g id="Layer_1" />
-                                    <g id="Layer_2">
-                                        <path
-                                            d="M50 2.5c-58.892 1.725-64.898 84.363-7.46 95h14.92c57.451-10.647 51.419-93.281-7.46-95z"
-                                            style="fill:#1877f2" />
-                                        <path
-                                            d="M57.46 64.104h11.125l2.117-13.814H57.46v-8.965c0-3.779 1.85-7.463 7.781-7.463h6.021V22.101c-12.894-2.323-28.385-1.616-28.722 17.66V50.29H30.417v13.814H42.54V97.5h14.92V64.104z"
-                                            style="fill:#f1f1f1" />
-                                    </g>
-                                </svg>
-                                <span>Facebook</span>
-                            </a>
                         </div>
                     </div>
                 </div>
