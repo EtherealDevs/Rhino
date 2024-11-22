b7e0f2a1284136799e2771440dd2337f6bfcd54d
joaccco <joaccofranc02@icloud.com>
2024-11-22 16:27:42 -0300

    bunny add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
     <!-- Fonts -->
-   {{--  <link rel="preconnect" href="https://fonts.bunny.net"> --}}
+    <link rel="preconnect" href="https://fonts.bunny.net">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
