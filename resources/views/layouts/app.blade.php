af4fb62dab228020274c8a6c78721afbd7fdb27a
joaccco <joaccofranc02@icloud.com>
2024-11-27 15:10:06 -0300

    without google tag manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,24 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-NK82L3HG');
-    </script>
-    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -44,11 +26,6 @@
 </head>
 
 <body class="font-sans antialiased">
-    <!-- Google Tag Manager (noscript) -->
-    {{-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NK82L3HG" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript> --}}
-    <!-- End Google Tag Manager (noscript) -->
-
     <div id="contain-loader"
         class="fixed z-50 flex h-screen w-screen backdrop-blur-2xl justify-center items-center bg-black/25">
         <div id="loader" class="h-18 mx-auto mt-12"></div>
