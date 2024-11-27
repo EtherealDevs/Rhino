603b1c6039754d84d8ed3bfd57e119bbae7a0013
joaccco <joaccofranc02@icloud.com>
2024-11-27 01:02:05 -0300

    reform view payment failure

--- resources/views/checkout/payment_failure.blade.php
+++ resources/views/checkout/payment_failure.blade.php
@@ -1,20 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <div
-        class="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
+        class="w-full h-screen items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
         <div class="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
             <p class="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-300 mt-2">Pago Rechazado</p>
-
-            <a href="/"
-                class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
-                title="Return Home">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-                <span>Return Home</span>
-            </a>
         </div>
         <div class="w-1/2 lg:h-full flex lg:items-end justify-center p-4">
             <!-- Aquí empieza la información del pago -->
@@ -126,6 +115,16 @@
                     </p>
                 </div>
             </div>
+            <a href="/"
+                class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded-xl transition duration-150"
+                title="Return Home">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+                        clip-rule="evenodd"></path>
+                </svg>
+                <span>Volver a Inicio</span>
+            </a>
             <!-- Fin de la información del pago -->
         </div>
     </div>
