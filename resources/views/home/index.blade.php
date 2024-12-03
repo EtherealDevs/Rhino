f28d4e0199a3d39507e58cef6d19d7db6006b44b
joaccco <joaccofranc02@icloud.com>
2024-12-03 19:14:22 -0300

    lazy

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -88,13 +88,13 @@
                                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
                                         <img src="/img/banners/1.webp"
                                             class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-                                            alt="...">
+                                            alt="..." loading="lazy">
                                     </div>
                                     <!-- Item 3 -->
                                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
                                         <img src="/img/banners/2.webp"
                                             class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-                                            alt="...">
+                                            alt="..." loading="lazy">
                                     </div>
                                     <!-- Item 2 -->
                                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
