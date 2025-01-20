305a27f72ad591ef243300f7668b77d13f92cf1f
joaccco <joaccofranc02@icloud.com>
2025-01-20 09:28:15 -0300

    Products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Seccion de productos')
+@section('title', 'Productos')
 
 @section('meta_description', 'Descubre la moda del futuro hoy mismo. Ropa de hombre y adolescente innovadora y exclusiva, diseñada para los que buscan un estilo único. Compra online las últimas tendencias y renueva tu armario sin salir de casa. #modainnovadora #ropasonline #estilopropio')
 @section('content')
