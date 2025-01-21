080304e2f4d6373e5b5fa77c35f113baf9185fd4
joaccco <joaccofranc02@icloud.com>
2025-01-21 12:22:58 -0300

    payment method

--- resources/views/livewire/checkout-config.blade.php
+++ resources/views/livewire/checkout-config.blade.php
@@ -191,12 +191,12 @@
 
                                 <h1>Seleccionemoslo aquí 👇🏼</h1>
                                 <!-- Radio Button Mercado Pago -->
-                               {{--  <div class="radio-item">
+                                <div class="radio-item">
 
                                     <input wire:model.live="payment" type="radio" id="mercado_pago"
                                         name="paymentMethod" x-model="paymentMethod" value="mercado_pago" />
                                     <label class="text-white" for="mercado_pago">Mercado Pago</label>
-                                </div> --}}
+                                </div>
 
                                 <!-- Radio Button Transferencia -->
                                 <div class="radio-item">
